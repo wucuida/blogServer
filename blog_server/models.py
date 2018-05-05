f020c07ded002a4424f2d02788df4fc0ca773f9a
wucuida <da727438548@qq.com>
2018-05-06 02:53:46 +0800

    完善model模块；忽略数据库文件

--- blog_server/models.py
+++ blog_server/models.py
@@ -20,22 +20,40 @@ class Article(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(64), index=True)
 	summary = db.Column(db.Text)
-	createTime = db.Column(db.Integer)
-	updateTime = db.Column(db.Integer)
+	create_time = db.Column(db.Integer)
+	update_time = db.Column(db.Integer)
 	tag = db.relationship("Tag", secondary=article_tag_map, lazy="subquery",
 		backref=db.backref("articles", lazy=True))
+
 	def __repr__(self):
 		return "Article (%s): %s" % (self.id, self.title)
+
+	@property
+	def serialize(self):
+		return {
+			"id": self.id,
+			"title": self.title,
+			"create_time": self.create_time,
+			"summary": self.summary
+		}
 
 class Tag(db.Model):
 	__tablename__ = "tags"
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(16), index=True)
-	createTime = db.Column(db.Integer)
-	updateTime = db.Column(db.Integer)
+	create_time = db.Column(db.Integer)
+	update_time = db.Column(db.Integer)
 
 	def __repr__(self):
 		return "Tag (%s): %s" % (self.id, self.name)
+
+	@property
+	def serialize(self):
+		return {
+			"id": self.id,
+			"name": self.name,
+			"create_time": self.create_time
+		}
 
 # class Binding(db.Model):
 # 	__tablename__ = "bindings"
