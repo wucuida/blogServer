a13b2056cb6595f519504968dc17d91e1b70e23f
wucuida <da727438548@qq.com>
2018-05-06 02:55:25 +0800

    完善基本路由

--- blog_server/__init__.py
+++ blog_server/__init__.py
@@ -5,10 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['file_root_path'] = "D:/gitHub/blogSite/src/articles"
+app.config["FILE_ROOT_PATH"] = "F:/workspace/blogSite/src/articles"
+# app.config['FILE_ROOT_PATH'] = "D:/gitHub/blogSite/src/articles"
 app.config['SECRET_KEY'] = 'obj_id__5462cdf80ddf1a071485d85e_this_is_a_key_90'
 app.config['AUTH_SALT'] = "a_auth_salt_4_1_7"
-app.config["TOKEN_EXPIRES_IN"] = 30*60
+app.config["TOKEN_EXPIRES_IN"] = 30*60*10
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- blog_server/views.py
+++ blog_server/views.py
@@ -1,23 +1,28 @@
 #coding: utf-8
+import os
 from datetime import datetime
-from .models import User
+import time
+import base64
+from functools import wraps
+from .models import User, Article, Tag
 from . import app
 from . import db
 from flask import jsonify
 from flask import request
+from flask import g
+from sqlalchemy import desc
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import SignatureExpired
 from itsdangerous import BadSignature
 from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
-
-@app.route("/a")
-def test():
-	return jsonify({"a": 1})
+from werkzeug import secure_filename
 
 def tokenAuth(fn):
 	@wraps(fn)
 	def decorator(*args, **kw):
+		if request.method == "GET":
+			return fn(*args, **kw)
 		if request.headers.get("access_token"):
 			s = Serializer(secret_key=app.config["SECRET_KEY"],
 				salt=app.config["AUTH_SALT"])
@@ -30,9 +35,9 @@ def tokenAuth(fn):
 			except:
 				return jsonify({'error': 'unknown', "message": 'unknown error'})
 			# 解析 token
-			if user_id in data:
+			if 'user_id' in data:
 				g.user_id = data["user_id"]
-				fn(*args, **kw)
+				return fn(*args, **kw)
 			else:
 				return jsonify({"error": "token_error", "message": 'token is invalid'})
 		else:
@@ -42,9 +47,7 @@ def tokenAuth(fn):
 @app.route("/api/auth", methods=["POST"])
 def get_token():
 	name = request.json.get("name")
-	print request.json.get("password")
 	passwd = request.json.get("password")[:-3]
-	print passwd
 	user = User.query.filter_by(name=name).first()
 	if user is None:
 		return jsonify({'error': 'user_not_exist', 'message': "the user don't exists"})
@@ -58,18 +61,164 @@ def get_token():
 	s = Serializer(secret_key=app.config["SECRET_KEY"],
 		salt=app.config["AUTH_SALT"],
 		expires_in=app.config["TOKEN_EXPIRES_IN"])
-	return s.dumps({"user_id": 'user_id'})
+	token = s.dumps({"user_id": user.id})
+	return jsonify({'result': token})
 
+@app.route("/api/upload", methods=["POST"])
+@tokenAuth
+def upload_file():
+	title = request.args["title"]
+	article_file = request.files["file"]
+	article_root = app.config["FILE_ROOT_PATH"]
+	b64_title = base64.b64encode(title.encode('utf-8'))
+	print b64_title, "b64_title"
+	article_name = secure_filename(b64_title + ".md")
+	file_path = os.path.join(article_root, article_name)
+	article_file.save(file_path)
+	return jsonify({'result': article_name})
+
+@app.route("/api/articles", methods=["GET", "POST"])
+@tokenAuth
+def handle_articles():
+	if request.method == "GET":
+		return get_articles()
+	elif request.method == "POST":
+		return create_article()
+
+def get_articles():
+	title = request.args.get("title", "")
+	skip = request.args.get("cursor", 0)
+	limit = request.args.get("limit", 10)
+	query = Article.query.order_by(desc(Article.create_time))
+	if title != "":
+		query = Article.query.filter(Article.title.like("%"+title+"%"))
+	total = query.count()
+	articles = query.offset(skip).limit(limit).all()
+	return jsonify({
+		"total": total,
+		"limit": limit,
+		"cursor": skip,
+		"result": [article.serialize for article in articles]
+		})
+
+def create_article():
+	title = request.json.get("title")
+	summary = request.json.get("summary")
+	if not (title and summary):
+		return jsonify({'error': 'param_error', 'message': 'request param error'})
+	tags = request.json.get('tags')
+	if Article.query.filter_by(title=title).first():
+		return jsonify({'error': 'resouce existed', "message": 'article title is existed'})
+	now = int(time.time())
+	article = Article(title=title, summary=summary, create_time=now, update_time=now)
+	for tag_id in tags:
+		tag = Tag.query.get(tag_id)
+		if tag:
+			article.tag.append(tag)
+	db.session.add(article)
+	db.session.commit()
+	return jsonify({'result': article.serialize})
+
+@app.route("/api/articles/<int:article_id>", methods=["GET", "DELETE"])
+@tokenAuth
+def handle_article(article_id):
+	article = Article.query.get(article_id)
+	if not article:
+		return jsonify({
+			"error": 'resource_not_exist',
+			"message": "resource not found"
+			})
+	if request.method == "GET":
+		return read_article(article)
+	elif request.method == "DELETE":
+		return delete_article(article)
+	
+
+def read_article(article):
+	b64_name = base64.b64encode(article.title.encode("utf-8"))
+	file_path = os.path.join(app.config["FILE_ROOT_PATH"], secure_filename(b64_name)+".md")
+	with open(file_path, "rb") as f:
+		content = f.read()
+	return jsonify({'result': content})
+
+def delete_article(article):
+	db.session.delete(article)
+	db.session.commit()
+	file_path = os.path.join(app.config["FILE_ROOT_PATH"], article.title+".md")
+	if os.path.exists(file_path):
+		os.remove(file_path)
+	return jsonify({'result': article.serialize})
+
+
+@app.route("/api/tags", methods=["GET", "POST"])
+@tokenAuth
+def handle_tags():
+	if request.method == "GET":
+		return get_tags()
+	elif request.method == 'POST':
+		return create_tag()
+
+def get_tags():
+	name = request.args.get("name", "")
+	skip = request.args.get("cursor", 0)
+	limit = request.args.get("limit", 10)
+	query = Tag.query
+	if name != "":
+		query = Tag.query.filter(Tag.name.like("%"+name+"%"))
+	total = query.count()
+	tags = query.offset(skip).limit(limit).all()
+	return jsonify({
+		"total": total,
+		"limit": limit,
+		"cursor": skip,
+		"result": [tag.serialize for tag in tags]
+		})
+
+def create_tag():
+	name = request.json.get('name')
+	now = int(time.time())
+	tag = Tag(name=name, create_time=now, update_time=now)
+	db.session.add(tag)
+	db.session.commit()
+	return jsonify({'result': tag.serialize})
+
+@app.route("/api/tags/<int:tag_id>", methods=['PUT', 'DELETE'])
+@tokenAuth
+def handle_tag(tag_id):
+	tag = Tag.query.get(tag_id)
+	if not tag:
+		return jsonify({
+			"error": 'resource_not_exist',
+			"message": "resource not found"
+			})
+	if request.method == "PUT":
+		return update_tag(tag)
+	elif request.method == "DELETE":
+		return delete_tag(tag)
+
+def update_tag(tag):
+	name = request.json.get('name')
+	if not name:
+		return jsonify({'error': 'param_error', 'message': 'request param error'})
+	tag.name = name
+	tag.update_time = int(time.time())
+	db.session.commit()
+	return jsonify({'result': tag.serialize})
+
+def delete_tag(tag):
+	db.session.delete(tag)
+	db.session.commit()
+	return jsonify({'result': tag.serialize})
 
 @app.route("/api/users", methods=["POST", "PUT"])
 def create_user():
 	p = '593bbd2b2780acdcb9953862898efcb7'
+	hash_passwd = generate_password_hash(p, salt_length=12)
 	if request.method == "POST":
-		user = User(name = 'wucd', passwd=p)
+		user = User(name = 'wucd', passwd=hash_passwd)
 		db.session.add(user)
 		db.session.commit()
-		return jsonify({'ok': '1'})
-	elif request.method == "PUT":
-		hash_passwd = generate_password_hash(p, salt_length=12)
+		return jsonify({'result': 'add ok'})
+	elif request.method == "PUT":	
 		User.query.filter(User.name=="wucd").update({'passwd': hash_passwd})
-		return jsonify({'ok': '2'})
+		return jsonify({'result': 'update ok'})
